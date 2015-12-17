495e5ffc89139f651542a9d88b2659a5c8b59633
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-12-17 16:51:19 +0000

    Added first api call

--- tenant_api/app/models/appuser.server.model.js
+++ tenant_api/app/models/appuser.server.model.js
@@ -1,11 +1,29 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var PropertySchema = new Schema ({
+  // _id: Number,
+  post_code: String
+  // street_name: String,
+  // landlord_name: String,
+  // landlord_contact_details: String,
+  // contract_start: Date,
+  // contract_end :Date,
+  // property_type: String,
+  // number_of_flatmates: String,
+  // monthly_cost: Number,
+  // deposit_amount: Number,
+  // inclusive: [Boolean]
+});
+
 var AppUserSchema = new Schema({
     name: {type: String, required: true },
     profile_picture: String,
-    properties: [Properties],
-    connections: [AppUser]
+    properties: [{ type: Number, ref: 'Property' }],
+    connections: [{ type: Schema.Types.ObjectId, ref: 'AppUser' }]
 });
 
-mongoose.model('AppUser', AppUserSchema);
+
+var AppUser = mongoose.model('AppUser', AppUserSchema);
+
+var Property = mongoose.model('Property', PropertySchema);
