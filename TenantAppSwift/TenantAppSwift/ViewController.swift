1d603529ed6ab3c1083bc38803d9006d0b9bd05a
jbhdeconinck <jbhdeconinck@gmail.com>
2015-12-18 17:40:13 +0000

    created get for properties

--- TenantAppSwift/TenantAppSwift/ViewController.swift
+++ TenantAppSwift/TenantAppSwift/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var propertyInfo = [String: AnyObject]()
+    
+    @IBOutlet weak var userPicture: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getUserPropertyId()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +25,92 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    // Retrieve user information
+    
+    func getUserPropertyId() {
+        
+        let path: String = "https://housematey.herokuapp.com/appusers/2/"
+        let url = NSURL(string: path)!
+        let session = NSURLSession.sharedSession()
+        
+        // Make the GET call
+        let task = session.dataTaskWithURL(url, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
+            
+            // Read the JSON
+            
+            do {
+                if let userString = NSString(data:data!, encoding: NSUTF8StringEncoding) {
+                     print(data)
+                
+                let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers ) as! NSDictionary
+                    let propertyID = jsonData["properties"]![0]
+                   
+                    print(propertyID)
+                    
+                    dispatch_async(dispatch_get_main_queue(),{
+                      self.getUserPropertyInfo(propertyID!)
+                    })
+                    
+                }
+                
+            } catch {
+                print("bad things happened")
+            }
+           
+        })
+        
+        task.resume()
+    }
+    
+    func getUserPropertyInfo(id: AnyObject) {
+        
+        let path: String = "https://housematey.herokuapp.com/properties/\(id)"
+        let url = NSURL(string: path)!
+        let session = NSURLSession.sharedSession()
+        
+        // Make the GET call
+        let task = session.dataTaskWithURL(url, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
+            
+            // Read the JSON
+            
+            do {
+                if let userString = NSString(data:data!, encoding: NSUTF8StringEncoding) {
+                    print(data)
+                    
+                    let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers ) as! NSDictionary
+                    
+                    print(jsonData["deposit_amount"])
+                    
+                    
+                }
+                
+            } catch {
+                print("bad things happened")
+            }
+            
+        })
+        
+        task.resume()
+    }
+    
 }
+
+
+
 
