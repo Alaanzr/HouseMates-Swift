54a9b5206e0089e0f3fa95ba32d915946236ecbe
Mattia46 <mattia.assogna@gmail.com>
2015-12-19 12:26:00 +0000

    first refactor for the get request using SwiftyJson

--- TenantAppSwift/TenantAppSwift/ViewController.swift
+++ TenantAppSwift/TenantAppSwift/ViewController.swift
@@ -10,107 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var propertyInfo = [String: AnyObject]()
-    
-    @IBOutlet weak var userPicture: UIImageView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getUserPropertyId()
+        print("ciao1")
+        getUser()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+      
+
         // Dispose of any resources that can be recreated.
     }
     
-    
-    // Retrieve user information
-    
-    func getUserPropertyId() {
+    func getUser() {
         
-        let path: String = "https://housematey.herokuapp.com/appusers/2/"
-        let url = NSURL(string: path)!
+        
+        let path = "https://housematey.herokuapp.com/appusers"
+        let url = NSURL(string: path)
         let session = NSURLSession.sharedSession()
-        
-        // Make the GET call
-        let task = session.dataTaskWithURL(url, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+        let task = session.dataTaskWithURL(url!) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            print(data)
+            print("ciao2")
             
+            let json = JSON(data: data!)
             
-            guard let realResponse = response as? NSHTTPURLResponse where
-                realResponse.statusCode == 200 else {
-                    print("Not a 200 response")
-                    return
-            }
-            
-            // Read the JSON
-            
-            do {
-                if let userString = NSString(data:data!, encoding: NSUTF8StringEncoding) {
-                     print(data)
-                
-                let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers ) as! NSDictionary
-                    let propertyID = jsonData["properties"]![0]
-                   
-                    print(propertyID)
-                    
-                    dispatch_async(dispatch_get_main_queue(),{
-                      self.getUserPropertyInfo(propertyID!)
-                    })
-                    
-                }
-                
-            } catch {
-                print("bad things happened")
-            }
-           
-        })
-        
+            let name = json[1]["name"]
+            print(name)
+       }
         task.resume()
     }
-    
-    func getUserPropertyInfo(id: AnyObject) {
-        
-        let path: String = "https://housematey.herokuapp.com/properties/\(id)"
-        let url = NSURL(string: path)!
-        let session = NSURLSession.sharedSession()
-        
-        // Make the GET call
-        let task = session.dataTaskWithURL(url, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-            
-            
-            guard let realResponse = response as? NSHTTPURLResponse where
-                realResponse.statusCode == 200 else {
-                    print("Not a 200 response")
-                    return
-            }
-            
-            // Read the JSON
-            
-            do {
-                if let userString = NSString(data:data!, encoding: NSUTF8StringEncoding) {
-                    print(data)
-                    
-                    let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers ) as! NSDictionary
-                    
-                    print(jsonData["deposit_amount"])
-                    
-                    
-                }
-                
-            } catch {
-                print("bad things happened")
-            }
-            
-        })
-        
-        task.resume()
-    }
-    
-}
-
-
+  }
 
 
