cb84673ee2ce7e905255d28294141ebc0561c923
jbhdeconinck <jbhdeconinck@gmail.com>
2015-12-26 13:43:09 +0000

    fixed views

---
+++ TenantAppSwift/TenantAppSwift/SignupViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignupViewController.swift
+//  TenantAppSwift
+//
+//  Created by Alix on 26/12/2015.
+//  Copyright © 2015 TenantTeam. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    
+    @IBOutlet weak var usernameSignUp: UITextField!
+    @IBOutlet weak var firstnameSignUp: UITextField!
+    @IBOutlet weak var lastnameSignUp: UITextField!
+    @IBOutlet weak var emailSignUp: UITextField!
+    @IBOutlet weak var passwordSignUp: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+     
+        
+    }
+    
+    @IBAction func signup(sender: UIButton) {
+        
+    }
+    
+
+}
--- TenantAppSwift/TenantAppSwift/ViewController.swift
+++ TenantAppSwift/TenantAppSwift/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Sign In text field outlets
+    
+    @IBOutlet weak var usernameSignIn: UITextField!
+    
+    @IBOutlet weak var passwordSignIn: UITextField!
+    
     
         override func viewDidLoad() {
             super.viewDidLoad()
-            createService().createUser()
-            createService().createProperty()
-
-            editService().editUser()
-            editService().editProperty()
-            getService().getUser()
-            deleteService().deleteUser()
+//            createService().createUser()
+//            createService().createProperty()
+//
+//            editService().editUser()
+//            editService().editProperty()
+//            getService().getUser()
+//            deleteService().deleteUser()
         
 
         }
@@ -28,6 +34,15 @@ class ViewController: UIViewController {
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
         }
+
+    
+    // Sign In action
+    
+    
+    @IBAction func signin(sender: UIButton) {
+        
+    }
+    
         
     }
 
